bdb718e744ead3538b000d217f06a5ee9bac81b1
Darya Kurilova <darya@cs.cmu.edu>
2014-06-04 12:38:46 +0100

    Added a class structure, refactored code.

--- sqpyte/interpreter.py
+++ sqpyte/interpreter.py
@@ -7,243 +7,146 @@ import capi
 
 testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.db")
 
-def opendb(db_name):
-    with rffi.scoped_str2charp(db_name) as db_name, lltype.scoped_alloc(capi.SQLITE3PP.TO, 1) as result:
-        errorcode = capi.sqlite3_open(db_name, result)
-        assert errorcode == 0
-        return rffi.cast(capi.SQLITE3P, result[0])
+class Sqlite3(object):
+    db = None
+    p = None
 
-def prepare(db, query):
-    length = len(query)
-    with rffi.scoped_str2charp(query) as query, lltype.scoped_alloc(rffi.VOIDPP.TO, 1) as result, lltype.scoped_alloc(rffi.CCHARPP.TO, 1) as unused_buffer:
-        errorcode = capi.sqlite3_prepare(db, query, length, result, unused_buffer)
-        assert errorcode == 0
-        return rffi.cast(capi.VDBEP, result[0])
+    def opendb(self, db_name):
+        with rffi.scoped_str2charp(db_name) as db_name, lltype.scoped_alloc(capi.SQLITE3PP.TO, 1) as result:
+            errorcode = capi.sqlite3_open(db_name, result)
+            assert errorcode == 0
+            self.db = rffi.cast(capi.SQLITE3P, result[0])
 
+    def prepare(self, query):
+        length = len(query)
+        with rffi.scoped_str2charp(query) as query, lltype.scoped_alloc(rffi.VOIDPP.TO, 1) as result, lltype.scoped_alloc(rffi.CCHARPP.TO, 1) as unused_buffer:
+            errorcode = capi.sqlite3_prepare(self.db, query, length, result, unused_buffer)
+            assert errorcode == 0
+            self.p = rffi.cast(capi.VDBEP, result[0])
 
-def allocateCursor(vdbe_struct, p1, nField, iDb, isBtreeCursor):
-    return capi.sqlite3_allocateCursor(vdbe_struct, p1, nField, iDb, isBtreeCursor) 
+    def python_OP_Init(self, pc, pOp):
+        if pOp.p2:
+            pc = pOp.p2 - 1
+        return pc
 
-def sqlite3VdbeMemIntegerify(pMem):
-    return capi.sqlite3_sqlite3VdbeMemIntegerify(pMem)
+    def python_OP_Rewind(self, pc, pOp):
+        with lltype.scoped_alloc(rffi.INTP.TO, 1) as internalPc:
+            rc = capi.impl_OP_Rewind(self.p, self.db, internalPc, pOp)
+            retPc = internalPc[0]
+        return retPc, rc
 
-def sqlite3BtreeCursor(p, iTable, wrFlag, pKeyInfo, pCur):
-    return capi.sqlite3_sqlite3BtreeCursor(p, iTable, wrFlag, pKeyInfo, pCur)
+    def python_OP_Transaction(self, pc, pOp):
+        return capi.impl_OP_Transaction(self.p, self.db, pc, pOp)
 
-def sqlite3BtreeCursorHints(btCursor, mask):
-    return capi.sqlite3_sqlite3BtreeCursorHints(btCursor, mask)
+    def python_OP_TableLock(self, pc, pOp):
+        return capi.impl_OP_TableLock(self.p, self.db, pc, pOp)
 
-def sqlite3VdbeSorterRewind(db, pC, res):
-    with lltype.scoped_alloc(rffi.INTP.TO, 1) as res:
-        errorcode = capi.sqlite3_sqlite3VdbeSorterRewind(db, pC, res)
-        return rffi.cast(capi.INTP, res[0])
-
-
-def python_OP_Init(p, db, pc, pOp):
-    if pOp.p2:
+    def python_OP_Goto(self, pc, pOp):
         pc = pOp.p2 - 1
-    return pc
+        return pc
 
-def python_OP_OpenRead_translated(p, db, pc, pOp):
-    assert(pOp.p5 & (CConfig.OPFLAG_P2ISREG | CConfig.OPFLAG_BULKCSR) == pOp.p5)
-    assert(pOp.opcode == CConfig.OP_OpenWrite or pOp.p5 == 0)
-    assert(p.bIsReader)
-    assert(pOp.opcode == CConfig.OP_OpenRead or p.readOnly == 0)
+    def python_OP_OpenRead(self, pc, pOp):
+        capi.impl_OP_OpenRead(self.p, self.db, pc, pOp)
 
-    if (p.expired):
-        rc = CConfig.SQLITE_ABORT
-        return
+    def python_OP_Column(self, pc, pOp):
+        return capi.impl_OP_Column(self.p, self.db, pc, pOp)
 
-    nField = 0
-    pKeyInfo = lltype.nullptr(capi.KEYINFO)
-    p2 = pOp.p2;
-    iDb = pOp.p3
-    wrFlag = 0
+    def python_OP_ResultRow(self, pc, pOp):
+        return capi.impl_OP_ResultRow(self.p, self.db, pc, pOp)
 
-    assert(iDb >= 0 and iDb < db.nDb)
-    #   assert( (p->btreeMask & (((yDbMask)1)<<iDb))!=0 );
+    def python_OP_Next(self, pc, pOp):
+        with lltype.scoped_alloc(rffi.INTP.TO, 1) as internalPc:
+            rc = capi.impl_OP_Rewind(self.p, self.db, internalPc, pOp)
+            retPc = internalPc[0]
+        return retPc, rc
 
-    pDb = db.aDb[iDb]
-    pX = pDb.pBt
+    def python_OP_Close(self, pc, pOp):
+        capi.impl_OP_Close(self.p, self.db, pc, pOp)
 
-    assert(not pX)
-
-    if pOp.opcode == CConfig.OP_OpenWrite:
-        wrFlag = 1
-        #     assert( sqlite3SchemaMutexHeld(db, iDb, 0) );
-        if pDb.pSchema.file_format < p.minWriteFileFormat:
-            p.minWriteFileFormat = pDb.pSchema.file_format
-        else:
-            wrFlag = 0
-
-    if pOp.p5 & CConfig.OPFLAG_P2ISREG:
-        assert(p2 > 0)
-        assert(p2 <= p.nMem - p.nCursor)
-        pIn2 = aMem[p2]
-        #     assert( memIsValid(pIn2) );
-        #     assert( (pIn2->flags & MEM_Int)!=0 );
-        sqlite3VdbeMemIntegerify(pIn2)
-        p2 = rffi.cast(rffi.INT, pIn2.u.i)
-        #     /* The p2 value always comes from a prior OP_CreateTable opcode and
-        #     ** that opcode will always set the p2 value to 2 or more or else fail.
-        #     ** If there were a failure, the prepared statement would have halted
-        #     ** before reaching this instruction. */
-        #     if( NEVER(p2<2) ) {
-        #       rc = SQLITE_CORRUPT_BKPT;
-        #       goto abort_due_to_error;
-        #     }
-        if pOp.p4type == CConfig.P4_KEYINFO:
-            pKeyInfo = pOp.p4.pKeyInfo
-            #     assert( pKeyInfo->enc==ENC(db) );
-            assert(pKeyInfo.db == db)
-            nField = intmask(pKeyInfo.nField) + intmask(pKeyInfo.nXField)
-        elif pOp.p4type == CConfig.P4_INT32:
-            nField = pOp.p4.i
-
-        assert(pOp.p1 >= 0)
-        assert(nField >= 0)
-        #   testcase( nField==0 );  /* Table with INTEGER PRIMARY KEY and nothing else */
-        pCur = allocateCursor(p, pOp.p1, nField, iDb, 1)
-        #   if( pCur==0 ) goto no_mem;
-        pCur.nullRow = 1
-        pCur.isOrdered = bool(1)
-        rc = sqlite3BtreeCursor(pX, p2, wrFlag, pKeyInfo, pCur.pCursor)
-        pCur.pKeyInfo = pKeyInfo
-        assert(CConfig.OPFLAG_BULKCSR == CConfig.BTREE_BULKLOAD)
-        sqlite3BtreeCursorHints(pCur.pCursor, (pOp.p5 & CConfig.OPFLAG_BULKCSR))
-
-        #   /* Since it performs no memory allocation or IO, the only value that
-        #   ** sqlite3BtreeCursor() may return is SQLITE_OK. */
-        assert(rc == CConfig.SQLITE_OK)
-        #   /* Set the VdbeCursor.isTable variable. Previous versions of
-        #   ** SQLite used to check if the root-page flags were sane at this point
-        #   ** and report database corruption if they were not, but this check has
-        #   ** since moved into the btree layer.  */
-        pCur.isTable = pOp.p4type != CConfig.P4_KEYINFO
+    def python_OP_Halt(self, pc, pOp):
+        with lltype.scoped_alloc(rffi.INTP.TO, 1) as internalPc:
+            rc = capi.impl_OP_Halt(self.p, self.db, internalPc, pOp)
+            retPc = internalPc[0]
+        return retPc, rc
 
 
-def python_OP_Rewind(p, db, pc, pOp):
-    with lltype.scoped_alloc(rffi.INTP.TO, 1) as internalPc:
-        rc = capi.impl_OP_Rewind(p, db, internalPc, pOp)
-        retPc = internalPc[0]
-    return retPc, rc
-
-def python_OP_Transaction(p, db, pc, pOp):
-    return capi.impl_OP_Transaction(p, db, pc, pOp)
-
-def python_OP_TableLock(p, db, pc, pOp):
-    return capi.impl_OP_TableLock(p, db, pc, pOp)
-
-def python_OP_Goto(p, db, pc, pOp):
-    pc = pOp.p2 - 1
-    return pc
-
-def python_OP_OpenRead(p, db, pc, pOp):
-    capi.impl_OP_OpenRead(p, db, pc, pOp)
-
-def python_OP_Column(p, db, pc, pOp):
-    return capi.impl_OP_Column(p, db, pc, pOp)
-
-def python_OP_ResultRow(p, db, pc, pOp):
-    return capi.impl_OP_ResultRow(p, db, pc, pOp)
-
-def python_OP_Next(p, db, pc, pOp):
-    with lltype.scoped_alloc(rffi.INTP.TO, 1) as internalPc:
-        rc = capi.impl_OP_Rewind(p, db, internalPc, pOp)
-        retPc = internalPc[0]
-    return retPc, rc
-
-def python_OP_Close(p, db, pc, pOp):
-    capi.impl_OP_Close(p, db, pc, pOp)
-
-def python_OP_Halt(p, db, pc, pOp):
-    with lltype.scoped_alloc(rffi.INTP.TO, 1) as internalPc:
-        rc = capi.impl_OP_Halt(p, db, internalPc, pOp)
-        retPc = internalPc[0]
-    return retPc, rc
+    def python_sqlite3_column_text(self, iCol):
+        return capi.sqlite3_column_text(self.p, iCol)
+    def python_sqlite3_column_bytes(self, iCol):
+        return capi.sqlite3_column_bytes(self.p, iCol)
 
 
-def python_sqlite3_column_text(p, iCol):
-    return capi.sqlite3_column_text(p, iCol)
-def python_sqlite3_column_bytes(p, iCol):
-    return capi.sqlite3_column_bytes(p, iCol)
+    def mainloop(self):
+        ops = self.p.aOp
+        rc = CConfig.SQLITE_OK
+        pc = self.p.pc
+        lastRowid = self.db.lastRowid
 
+        while rc == CConfig.SQLITE_OK:
+            pOp = ops[pc]
 
-def mainloop(vdbe_struct):
-    length = vdbe_struct.nOp
-    ops = vdbe_struct.aOp
-    rc = CConfig.SQLITE_OK
-    db = vdbe_struct.db
-    p = vdbe_struct
-    aMem = p.aMem
-    pc = p.pc
-    rc = 0
-    lastRowid = db.lastRowid
-
-    while rc == CConfig.SQLITE_OK:
-        pOp = ops[pc]
-
-        if pOp.opcode == CConfig.OP_Init:
-            print 'OP_Init'
-            pc = python_OP_Init(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_OpenRead or pOp.opcode == CConfig.OP_OpenWrite:
-            print 'OP_OpenRead'
-            python_OP_OpenRead(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_Rewind:
-            print 'OP_Rewind'
-            tmp = pc
-            pc, rc = python_OP_Rewind(p, db, pc, pOp)
-            if pc < 0:
-                pc = tmp
-            print 'pc = %s and rc = %s' % (pc, rc)
-        elif pOp.opcode == CConfig.OP_Transaction:
-            print 'OP_Transaction'
-            rc = python_OP_Transaction(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_TableLock:
-            print 'OP_TableLock'
-            rc = python_OP_TableLock(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_Goto:
-            print 'OP_Goto'
-            pc = python_OP_Goto(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_Column:
-            print 'OP_Column'
-            rc = python_OP_Column(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_ResultRow:
-            print 'OP_ResultRow'
-            rc = python_OP_ResultRow(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_Next:
-            print 'OP_Next'
-            tmp = pc
-            pc, rc = python_OP_Next(p, db, pc, pOp)
-            if pc < 0:
-                pc = tmp
-            print 'pc = %s and rc = %s' % (pc, rc)
-        elif pOp.opcode == CConfig.OP_Close:
-            print 'OP_Close'
-            python_OP_Close(p, db, pc, pOp)
-        elif pOp.opcode == CConfig.OP_Halt:
-            print 'OP_Halt'
-            tmp = pc
-            pc, rc = python_OP_Halt(p, db, pc, pOp)
-            if pc < 0:
-                pc = tmp
-            print 'pc = %s and rc = %s' % (pc, rc)
-        else:
-            print 'Opcode %s is not there yet!' % pOp.opcode
-            # raise Exception("Unimplemented bytecode %s." % pOp.opcode)
-            pass
-        print rc
-        print pc
-        pc += 1
-        if rc == CConfig.SQLITE_DONE:
-            break
-    return rc
+            if pOp.opcode == CConfig.OP_Init:
+                print 'OP_Init'
+                pc = self.python_OP_Init(pc, pOp)
+            elif pOp.opcode == CConfig.OP_OpenRead or pOp.opcode == CConfig.OP_OpenWrite:
+                print 'OP_OpenRead'
+                self.python_OP_OpenRead(pc, pOp)
+            elif pOp.opcode == CConfig.OP_Rewind:
+                print 'OP_Rewind'
+                tmp = pc
+                pc, rc = self.python_OP_Rewind(pc, pOp)
+                if pc < 0:
+                    pc = tmp
+                print 'pc = %s and rc = %s' % (pc, rc)
+            elif pOp.opcode == CConfig.OP_Transaction:
+                print 'OP_Transaction'
+                rc = self.python_OP_Transaction(pc, pOp)
+            elif pOp.opcode == CConfig.OP_TableLock:
+                print 'OP_TableLock'
+                rc = self.python_OP_TableLock(pc, pOp)
+            elif pOp.opcode == CConfig.OP_Goto:
+                print 'OP_Goto'
+                pc = self.python_OP_Goto(pc, pOp)
+            elif pOp.opcode == CConfig.OP_Column:
+                print 'OP_Column'
+                rc = self.python_OP_Column(pc, pOp)
+            elif pOp.opcode == CConfig.OP_ResultRow:
+                print 'OP_ResultRow'
+                rc = self.python_OP_ResultRow(pc, pOp)
+            elif pOp.opcode == CConfig.OP_Next:
+                print 'OP_Next'
+                tmp = pc
+                pc, rc = self.python_OP_Next(pc, pOp)
+                if pc < 0:
+                    pc = tmp
+                print 'pc = %s and rc = %s' % (pc, rc)
+            elif pOp.opcode == CConfig.OP_Close:
+                print 'OP_Close'
+                self.python_OP_Close(pc, pOp)
+            elif pOp.opcode == CConfig.OP_Halt:
+                print 'OP_Halt'
+                tmp = pc
+                pc, rc = self.python_OP_Halt(pc, pOp)
+                if pc < 0:
+                    pc = tmp
+                print 'pc = %s and rc = %s' % (pc, rc)
+            else:
+                print 'Opcode %s is not there yet!' % pOp.opcode
+                # raise Exception("Unimplemented bytecode %s." % pOp.opcode)
+                pass
+            print rc
+            print pc
+            pc += 1
+            if rc == CConfig.SQLITE_DONE:
+                break
+        return rc
 
 
 def run():
-    db = opendb(testdb)
-    ops = prepare(db, 'select name from contacts;')
-    mainloop(ops)
+    sqlite3 = Sqlite3()
+    sqlite3.opendb(testdb)
+    sqlite3.prepare('select name from contacts;')
+    sqlite3.mainloop()
 
 def entry_point(argv):
     run()
--- sqpyte/test/test_interpreter.py
+++ sqpyte/test/test_interpreter.py
@@ -1,6 +1,5 @@
 from rpython.rtyper.lltypesystem import rffi
-from sqpyte.interpreter import opendb, prepare, mainloop, allocateCursor, python_sqlite3_column_text, python_sqlite3_column_bytes
-from sqpyte.interpreter import sqlite3VdbeMemIntegerify, sqlite3BtreeCursor, sqlite3BtreeCursorHints, sqlite3VdbeSorterRewind
+from sqpyte.interpreter import Sqlite3
 from sqpyte.capi import CConfig
 from sqpyte import capi
 import os, sys
@@ -8,109 +7,112 @@ import os, sys
 testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.db")
 
 def test_opendb():
-    db = opendb(testdb)
-    assert db
+    sqlite3 = Sqlite3()
+    sqlite3.opendb(testdb)
+    assert sqlite3.db
 
 def test_prepare():
-    db = opendb(testdb)
-    p = prepare(db, 'select * from contacts;')
-    assert p and db
-    assert p.db == db
-    assert p.nOp == 17
+    sqlite3 = Sqlite3()
+    sqlite3.opendb(testdb)
+    sqlite3.prepare('select * from contacts;')
+    assert sqlite3.p and sqlite3.db
+    assert sqlite3.p.db == sqlite3.db
+    assert sqlite3.p.nOp == 17
 
-    assert p.aOp[0].opcode == 155
-    assert p.aOp[0].p1 == 0
-    assert p.aOp[0].p2 == 14
-    assert p.aOp[0].p3 == 0
+    assert sqlite3.p.aOp[0].opcode == 155
+    assert sqlite3.p.aOp[0].p1 == 0
+    assert sqlite3.p.aOp[0].p2 == 14
+    assert sqlite3.p.aOp[0].p3 == 0
 
-    assert p.aOp[1].opcode == 52
-    assert p.aOp[1].p1 == 0
-    assert p.aOp[1].p2 == 2
-    assert p.aOp[1].p3 == 0
+    assert sqlite3.p.aOp[1].opcode == 52
+    assert sqlite3.p.aOp[1].p1 == 0
+    assert sqlite3.p.aOp[1].p2 == 2
+    assert sqlite3.p.aOp[1].p3 == 0
 
-    assert p.aOp[2].opcode == 105
-    assert p.aOp[2].p1 == 0
-    assert p.aOp[2].p2 == 12
-    assert p.aOp[2].p3 == 0
+    assert sqlite3.p.aOp[2].opcode == 105
+    assert sqlite3.p.aOp[2].p1 == 0
+    assert sqlite3.p.aOp[2].p2 == 12
+    assert sqlite3.p.aOp[2].p3 == 0
 
 def test_mainloop():
-    db = opendb(testdb)
-    p = prepare(db, 'select name from contacts;')
-    rc = mainloop(p)
-    textlen = python_sqlite3_column_bytes(p, 0)
-    print python_sqlite3_column_bytes(p, 0)
-    print rffi.charpsize2str(rffi.cast(rffi.CCHARP, python_sqlite3_column_text(p, 0)), textlen)
-    rc = mainloop(p)
-    textlen = python_sqlite3_column_bytes(p, 2)
-    print python_sqlite3_column_bytes(p, 2)
-    print rffi.charpsize2str(rffi.cast(rffi.CCHARP, python_sqlite3_column_text(p, 2)), textlen)
+    sqlite3 = Sqlite3()
+    sqlite3.opendb(testdb)
+    sqlite3.prepare('select name from contacts;')
+    rc = sqlite3.mainloop()
+    textlen = sqlite3.python_sqlite3_column_bytes(0)
+    print sqlite3.python_sqlite3_column_bytes(0)
+    print rffi.charpsize2str(rffi.cast(rffi.CCHARP, sqlite3.python_sqlite3_column_text(0)), textlen)
+    rc = sqlite3.mainloop()
+    textlen = sqlite3.python_sqlite3_column_bytes(2)
+    print sqlite3.python_sqlite3_column_bytes(2)
+    print rffi.charpsize2str(rffi.cast(rffi.CCHARP, sqlite3.python_sqlite3_column_text(2)), textlen)
     # while rc == CConfig.SQLITE_ROW:
     #     textlen = python_sqlite3_column_bytes(p, 0)
     #     print python_sqlite3_column_bytes(p, 0)
     #     print rffi.charpsize2str(rffi.cast(rffi.CCHARP, python_sqlite3_column_text(p, 0)), textlen)
     #     rc = mainloop(p)
 
-def test_allocateCursor():
-    db = opendb(testdb)
-    p = prepare(db, 'select name from contacts;')
-    vdbe = allocateCursor(p, p.aOp[0].p1, p.aOp[0].p4.i, p.aOp[0].p3, 1)
 
-def test_sqlite3VdbeMemIntegerify():
-    db = opendb(testdb)
-    p = prepare(db, 'select name from contacts;')
-    pOp = p.aOp[0]
-    p2 = pOp.p2
-    aMem = p.aMem
-    pMem = aMem[p2]
-    rc = sqlite3VdbeMemIntegerify(pMem)
-    assert(rc == CConfig.SQLITE_OK)
 
-def test_sqlite3BtreeCursor():
-    db = opendb(testdb)
-    p = prepare(db, 'select name from contacts;')
-    pOp = p.aOp[0]
-    p2 = pOp.p2
-    iDb = pOp.p3
-    pDb = db.aDb[iDb]
-    pX = pDb.pBt
-    wrFlag = 1
-    pKeyInfo = pOp.p4.pKeyInfo
-    nField = p.aOp[0].p4.i
-    pCur = allocateCursor(p, pOp.p1, nField, iDb, 1)
-    pCur.nullRow = rffi.r_uchar(1)
-    pCur.isOrdered = bool(1)
-    rc = sqlite3BtreeCursor(pX, p2, wrFlag, pKeyInfo, pCur.pCursor)
-    assert(rc == CConfig.SQLITE_OK)
+# def test_allocateCursor():
+#     db = opendb(testdb)
+#     p = prepare(db, 'select name from contacts;')
+#     vdbe = allocateCursor(p, p.aOp[0].p1, p.aOp[0].p4.i, p.aOp[0].p3, 1)
 
-def test_sqlite3BtreeCursorHints():
-    db = opendb(testdb)
-    p = prepare(db, 'select name from contacts;')
-    pOp = p.aOp[0]
-    iDb = pOp.p3
-    nField = p.aOp[0].p4.i
-    pCur = allocateCursor(p, pOp.p1, nField, iDb, 1)
-    sqlite3BtreeCursorHints(pCur.pCursor, (pOp.p5 & CConfig.OPFLAG_BULKCSR))
+# def test_sqlite3VdbeMemIntegerify():
+#     db = opendb(testdb)
+#     p = prepare(db, 'select name from contacts;')
+#     pOp = p.aOp[0]
+#     p2 = pOp.p2
+#     aMem = p.aMem
+#     pMem = aMem[p2]
+#     rc = sqlite3VdbeMemIntegerify(pMem)
+#     assert(rc == CConfig.SQLITE_OK)
 
-def test_sqlite3VdbeSorterRewind():
-    db = opendb(testdb)
-    p = prepare(db, 'select name from contacts;')
-    pOp = p.aOp[0]
-    p2 = pOp.p2
-    iDb = pOp.p3
-    pDb = db.aDb[iDb]
-    pX = pDb.pBt
-    wrFlag = 1
-    pKeyInfo = pOp.p4.pKeyInfo
-    nField = p.aOp[0].p4.i
-    pCur = allocateCursor(p, pOp.p1, nField, iDb, 1)
-    pCur.nullRow = rffi.r_uchar(1)
-    pCur.isOrdered = bool(1)
-    rc = sqlite3BtreeCursor(pX, p2, wrFlag, pKeyInfo, pCur.pCursor)
-    sqlite3BtreeCursorHints(pCur.pCursor, (pOp.p5 & CConfig.OPFLAG_BULKCSR))
-    pC = p.apCsr[pOp.p1]
-    res = 1
-    rc = sqlite3VdbeSorterRewind(db, pC, res)
-    assert(rc == CConfig.SQLITE_OK)
+# def test_sqlite3BtreeCursor():
+#     db = opendb(testdb)
+#     p = prepare(db, 'select name from contacts;')
+#     pOp = p.aOp[0]
+#     p2 = pOp.p2
+#     iDb = pOp.p3
+#     pDb = db.aDb[iDb]
+#     pX = pDb.pBt
+#     wrFlag = 1
+#     pKeyInfo = pOp.p4.pKeyInfo
+#     nField = p.aOp[0].p4.i
+#     pCur = allocateCursor(p, pOp.p1, nField, iDb, 1)
+#     pCur.nullRow = rffi.r_uchar(1)
+#     pCur.isOrdered = bool(1)
+#     rc = sqlite3BtreeCursor(pX, p2, wrFlag, pKeyInfo, pCur.pCursor)
+#     assert(rc == CConfig.SQLITE_OK)
 
-def test_sqpyte_test_function():
-    assert capi.sqpyte_test_function(1) == 2
+# def test_sqlite3BtreeCursorHints():
+#     db = opendb(testdb)
+#     p = prepare(db, 'select name from contacts;')
+#     pOp = p.aOp[0]
+#     iDb = pOp.p3
+#     nField = p.aOp[0].p4.i
+#     pCur = allocateCursor(p, pOp.p1, nField, iDb, 1)
+#     sqlite3BtreeCursorHints(pCur.pCursor, (pOp.p5 & CConfig.OPFLAG_BULKCSR))
+
+# def test_sqlite3VdbeSorterRewind():
+#     db = opendb(testdb)
+#     p = prepare(db, 'select name from contacts;')
+#     pOp = p.aOp[0]
+#     p2 = pOp.p2
+#     iDb = pOp.p3
+#     pDb = db.aDb[iDb]
+#     pX = pDb.pBt
+#     wrFlag = 1
+#     pKeyInfo = pOp.p4.pKeyInfo
+#     nField = p.aOp[0].p4.i
+#     pCur = allocateCursor(p, pOp.p1, nField, iDb, 1)
+#     pCur.nullRow = rffi.r_uchar(1)
+#     pCur.isOrdered = bool(1)
+#     rc = sqlite3BtreeCursor(pX, p2, wrFlag, pKeyInfo, pCur.pCursor)
+#     sqlite3BtreeCursorHints(pCur.pCursor, (pOp.p5 & CConfig.OPFLAG_BULKCSR))
+#     pC = p.apCsr[pOp.p1]
+#     res = 1
+#     rc = sqlite3VdbeSorterRewind(db, pC, res)
+#     assert(rc == CConfig.SQLITE_OK)
+
