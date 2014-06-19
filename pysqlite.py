b14b7d182dcda2254f5ed7f64ecf89706a154d2d
Darya Kurilova <darya@cs.cmu.edu>
2014-06-19 16:04:59 +0100

    Simple Python program now takes a query as an argument.
    Former-commit-id: 48ac2e71f625ac1ba35ba2990e569f65f47f2b22

--- pysqlite.py
+++ pysqlite.py
@@ -5,23 +5,35 @@ import time
 
 testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sqpyte/test/test.db")
 
-def run(cursor):
-    query = 'select first_name from people where age > 1;'
+def run(cursor, query):
     i = 0
     for name, in cursor.execute(query):
         i += len(name)
     return i
 
-
-def main_work():
+def main_work(query):
     connection = sqlite3.connect(testdb)
     cursor = connection.cursor()
     for i in range(2):
-        run(cursor)
+        run(cursor, query)
 
     t1 = time.time()
-    print run(cursor)
+    print run(cursor, query)
     t2 = time.time()
     print t2 - t1
 
-main_work()
+def entry_point(argv):
+    try:
+        query = argv[1]
+    except IndexError:
+        print "You must supply a query to be run: e.g., 'select first_name from people where age > 1;'."
+        return 1
+    
+    main_work(query)
+    return 0
+
+def target(*args):
+    return entry_point
+    
+if __name__ == "__main__":
+    entry_point(sys.argv)
