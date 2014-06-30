347222dc89cddc29ebfeb026d1179a06580b3e17
Darya Kurilova <darya@cs.cmu.edu>
2014-06-30 15:44:01 +0100

    Latest target.
    Former-commit-id: f9544f03cbf0c5ad3ac0aaafdd93a9650ba8c934

--- target.py
+++ target.py
@@ -3,7 +3,8 @@ from sqpyte.interpreter import Sqlite3DB, Sqlite3Query
 from rpython.rlib import jit
 from sqpyte.capi import CConfig
 
-testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sqpyte/test/test.db")
+# testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sqpyte/test/test.db")
+testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sqpyte/test/tpch.db")
 assert os.path.isfile(testdb)
 
 jitdriver = jit.JitDriver(
@@ -24,14 +25,16 @@ def run(query):
     return i
 
 def entry_point(argv):
-    try:
-        query = argv[1]
-    except IndexError:
-        print "You must supply a query to be run: e.g., 'select first_name from people where age > 1;'."
-        return 1
+    # try:
+    #     query = argv[1]
+    # except IndexError:
+    #     print "You must supply a query to be run: e.g., 'select first_name from people where age > 1;'."
+    #     return 1
+
+    queryStr = "select sum(l.extendedprice * l.discount) as revenue from lineitem l where l.shipdate >= date('1996-01-01') and l.shipdate < date('1996-01-01', '+1 year') and l.discount between 0.04 and 0.07 and l.quantity < 25;"
 
     db = Sqlite3DB(testdb).db
-    query = Sqlite3Query(db, query)
+    query = Sqlite3Query(db, queryStr)
     
     for i in range(2):
         run(query)
