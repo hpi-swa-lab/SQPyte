007a244f10c2b0edd5938143c7f4d606a2f06525
Darya Kurilova <darya@cs.cmu.edu>
2014-07-07 11:27:46 +0100

    Added tests for queries 17 and 19. Currently not passing.
    Former-commit-id: 541bb56a8c1ee28e86e31ad66e7be6984caa0ab9

--- sqpyte/test/test_interpreter.py
+++ sqpyte/test/test_interpreter.py
@@ -154,6 +154,82 @@ def test_query_14():
     result = rffi.charpsize2str(rffi.cast(rffi.CCHARP, query.python_sqlite3_column_text(0)), textlen)
     assert float(result) == 15.9871053076363
 
+def test_query_17():
+    db = Sqlite3DB(tpchdb).db
+    queryStr = ("select "
+                    "sum(l.extendedprice) / 7.0 as avg_yearly "
+                "from "
+                    "lineitem l, "
+                    "part p "
+                "where "
+                    "p.partkey = l.partkey "
+                    "and p.brand = 'Brand#45' "
+                    "and p.container = 'SM PKG' "
+                    "and l.quantity < ( "
+                        "select "
+                            "0.2 * avg(quantity) "
+                        "from "
+                            "lineitem "
+                        "where "
+                            "partkey <> p.partkey "
+                    ");"
+        )
+    query = Sqlite3Query(db, queryStr)
+    rc = query.mainloop()
+    assert rc == CConfig.SQLITE_ROW
+    textlen = query.python_sqlite3_column_bytes(0)
+    result = rffi.charpsize2str(rffi.cast(rffi.CCHARP, query.python_sqlite3_column_text(0)), textlen)
+    print textlen
+    print result
+    assert float(result) == 3655.76571428571
+
+def test_query_19():
+    db = Sqlite3DB(tpchdb).db
+    queryStr = ("select "
+                    "sum(l.extendedprice* (1 - l.discount)) as revenue "
+                "from "
+                    "lineitem l, "
+                    "part p "
+                "where "
+                    "( "
+                        "p.partkey = l.partkey "
+                        "and p.brand = 'Brand#12' "
+                        "and p.container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG') "
+                        "and l.quantity >= 1 and l.quantity <= 1 + 10 "
+                        "and p.size between 1 and 5 "
+                        "and l.shipmode in ('AIR', 'AIR REG') "
+                        "and l.shipinstruct = 'DELIVER IN PERSON' "
+                    ") "
+                    "or "
+                    "( "
+                        "p.partkey = l.partkey "
+                        "and p.brand = 'Brand#23' "
+                        "and p.container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK') "
+                        "and l.quantity >= 10 and l.quantity <= 10 + 10 "
+                        "and p.size between 1 and 10 "
+                        "and l.shipmode in ('AIR', 'AIR REG') "
+                        "and l.shipinstruct = 'DELIVER IN PERSON' "
+                    ") "
+                    "or "
+                    "( "
+                        "p.partkey = l.partkey "
+                        "and p.brand = 'Brand#34' "
+                        "and p.container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG') "
+                        "and l.quantity >= 20 and l.quantity <= 20 + 10 "
+                        "and p.size between 1 and 15 "
+                        "and l.shipmode in ('AIR', 'AIR REG') "
+                        "and l.shipinstruct = 'DELIVER IN PERSON' "
+                    ");"
+        )
+    query = Sqlite3Query(db, queryStr)
+    rc = query.mainloop()
+    assert rc == CConfig.SQLITE_ROW
+    textlen = query.python_sqlite3_column_bytes(0)
+    result = rffi.charpsize2str(rffi.cast(rffi.CCHARP, query.python_sqlite3_column_text(0)), textlen)
+    print textlen
+    print result
+    assert float(result) == 22923.028
+
 def test_translated_allocateCursor():
     db = Sqlite3DB(testdb).db
     p = Sqlite3Query(db, 'select name from contacts;').p
