2744e255a5e0e7ee19d197cbfba155f75faddefa
Darya Kurilova <darya@cs.cmu.edu>
2014-07-08 12:30:17 +0100

    Target takes in a query file and allows to compare with sample output (currently works only for count queries).
    Former-commit-id: f364688c8e7a450190bb215ab48e8e5472f6e20c

--- target.py
+++ target.py
@@ -2,6 +2,7 @@ import sys, os, time
 from sqpyte.interpreter import Sqlite3DB, Sqlite3Query, SQPyteException
 from rpython.rlib import jit
 from sqpyte.capi import CConfig
+from rpython.rtyper.lltypesystem import rffi
 
 # testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sqpyte/test/test.db")
 testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sqpyte/test/tpch.db")
@@ -13,39 +14,71 @@ jitdriver = jit.JitDriver(
     )
     # get_printable_location=get_printable_location)
 
-def run(query):
-    try:
-        query.reset_query()
+def run(query, queryRes):
+    query.reset_query()
+    rc = query.mainloop()
+    i = 0
+    textlen = 0
+    while rc == CConfig.SQLITE_ROW:
+        jitdriver.jit_merge_point(i=i, query=query, rc=rc)
+        textlen = query.python_sqlite3_column_bytes(0)
         rc = query.mainloop()
-        i = 0
-        while rc == CConfig.SQLITE_ROW:
-            jitdriver.jit_merge_point(i=i, query=query, rc=rc)
-            textlen = query.python_sqlite3_column_bytes(0)
-            rc = query.mainloop()
-            i += textlen
-        return i
-    except SQPyteException, e:
-        print "ERROR!", e.msg
-        raise
+        i += textlen
+    result = rffi.charpsize2str(rffi.cast(rffi.CCHARP, query.python_sqlite3_column_text(0)), textlen)
+    if queryRes != "":
+        print 'Expected result:\n%s' % queryRes
+    return result
+
+    # try:
+    #     query.reset_query()
+    #     rc = query.mainloop()
+    #     i = 0
+    #     while rc == CConfig.SQLITE_ROW:
+    #         jitdriver.jit_merge_point(i=i, query=query, rc=rc)
+    #         textlen = query.python_sqlite3_column_bytes(0)
+    #         rc = query.mainloop()
+    #         i += textlen
+    #     return i
+    # except SQPyteException, e:
+    #     print "ERROR!", e.msg
+    #     raise
 
 def entry_point(argv):
-    # try:
-    #     query = argv[1]
-    # except IndexError:
-    #     print "You must supply a query to be run: e.g., 'select first_name from people where age > 1;'."
-    #     return 1
+    try:
+        queryPath = argv[1]
+    except IndexError:
+        print "You must supply a file with query to be run."
+        return 1
 
-    queryStr = "select sum(l.extendedprice * l.discount) as revenue from lineitem l where l.shipdate >= date('1996-01-01') and l.shipdate < date('1996-01-01', '+1 year') and l.discount between 0.04 and 0.07 and l.quantity < 25;"
+    fp = os.open(queryPath, os.O_RDONLY, 0777)
+    queryStr = ""
+    while True:
+        read = os.read(fp, 4096)
+        if len(read) == 0:
+            break
+        queryStr += read
+    os.close(fp)
+
+    queryRes = ""
+    if len(argv) > 2:
+        queryResPath = argv[2]
+        fp = os.open(queryResPath, os.O_RDONLY, 0777)
+        while True:
+            read = os.read(fp, 4096)
+            if len(read) == 0:
+                break
+            queryRes += read
+        os.close(fp)
 
     db = Sqlite3DB(testdb).db
     query = Sqlite3Query(db, queryStr)
     
     for i in range(2):
-        run(query)
+        run(query, "")
     t1 = time.time()
-    print run(query)
+    print 'Query result:\n%s\n' % run(query, queryRes)
     t2 = time.time()
-    print "%ss" % (t2 - t1)
+    print "%s" % (t2 - t1)
     return 0
 
 def target(*args):
