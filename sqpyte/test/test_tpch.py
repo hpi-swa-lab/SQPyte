a0670d50ea29036fddab9230e838f23b6c3dd44e
Darya Kurilova <darya@cs.cmu.edu>
2014-07-18 20:00:45 +0100

    Added test for query 20. Can't run using pytest.py as initialisation is not done in full and therefore segfaults. Use target.py instead.
    Former-commit-id: 45e9dd242eb783734f0c5aae5cd9359c45d26076

--- sqpyte/test/test_tpch.py
+++ sqpyte/test/test_tpch.py
@@ -135,3 +135,52 @@ def test_query_19():
     print result
     assert float(result) == 22923.028
 
+def test_query_20():
+    db = Sqlite3DB(tpchdb).db
+    queryStr = ("select "
+                    "s.name, "
+                    "s.address "
+                "from "
+                    "supplier s, "
+                    "nation n "
+                "where "
+                    "s.suppkey in ( "
+                        "select "
+                            "ps.suppkey "
+                        "from "
+                            "partsupp ps "
+                        "where "
+                            "ps.partkey in ( "
+                                "select "
+                                    "partkey "
+                                "from "
+                                    "part "
+                                "where "
+                                    "name like 'b%' "
+                            ") "
+                            "and ps.availqty > ( "
+                                "select "
+                                    "0.5 * sum(l.quantity) "
+                                "from "
+                                    "lineitem l "
+                                "where "
+                                    "l.partkey = ps.partkey "
+                                    "and l.suppkey = ps.suppkey "
+                                    "and l.shipdate >= date('1995-01-01') "
+                                    "and l.shipdate < date('1995-01-01', '+1 year') "
+                            ") "
+                    ") "
+                    "and s.nationkey = n.nationkey "
+                    "and n.name = 'BRAZIL' "
+                "order by "
+                    "s.name; "
+        )
+    query = Sqlite3Query(db, queryStr)
+    rc = query.mainloop()
+    assert rc == CConfig.SQLITE_ROW
+    textlen = query.python_sqlite3_column_bytes(0)
+    result = rffi.charpsize2str(rffi.cast(rffi.CCHARP, query.python_sqlite3_column_text(0)), textlen)
+    print textlen
+    print result
+    assert float(result) == 22923.028
+
