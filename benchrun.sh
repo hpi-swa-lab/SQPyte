92d3cbcffbd338f275d08e87d737a5616981524e
Darya Kurilova <darya@cs.cmu.edu>
2014-07-21 21:13:50 +0100

    Changed testing and benchmarking scripts to pass path to DB to target.
    Former-commit-id: 5f8278b201982aeb0d5153274eb45af20c8d8ef0

--- benchrun.sh
+++ benchrun.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 DIR=$(pwd)
+DB=$DIR/sqpyte/test/big-tpch.db
 
 for i in {1..14} {16..22}
 do
     echo "========== Query $i =========="
     for j in {1..10}
     do
-        ./target-c -b $DIR/tpch/sqlite-queries-in/$i.sql
+        ./target-c -b $DB $DIR/tpch/sqlite-queries-in/$i.sql
     done
 done
--- testrun.sh
+++ testrun.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 DIR=$(pwd)
+DB=$DIR/sqpyte/test/tpch.db
 
 mkdir $DIR/tmp
 
 for i in {1..22}
 do
-    ./target-c -t $DIR/tpch/sqlite-queries-in/$i.sql > $DIR/tmp/$i.out
+    ./target-c -t $DB $DIR/tpch/sqlite-queries-in/$i.sql > $DIR/tmp/$i.out
 
     DIFF=$(diff $DIR/tmp/$i.out $DIR/tpch/sqlite-queries-out/$i.txt) 
     if [ "$DIFF" == "" ]; then
         echo -e "Query $i:\tPass"
     else
         echo -e "Query $i:\tFAIL"
-        HG=$(hg st $DIR/sqpyte/test/tpch.db)
+        HG=$(hg st $DB)
         if [ "$HG" != "" ]; then
-            hg revert $DIR/sqpyte/test/tpch.db
-            rm $DIR/sqpyte/test/tpch.db.orig
+            hg revert $DB
+            rm $DB.orig
         fi
     fi
 done
