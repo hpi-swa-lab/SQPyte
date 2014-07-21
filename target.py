b3eb24c94ba3fff28167cb38158afce364194ead
Darya Kurilova <darya@cs.cmu.edu>
2014-07-21 10:40:48 +0100

    Added printRes to jitdriver annotations.
    Former-commit-id: 6921f785a39fd1e71d858a436b5e4cb1d02735d4

--- target.py
+++ target.py
@@ -11,7 +11,7 @@ testdb = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sqpyte/test/t
 assert os.path.isfile(testdb)
 
 jitdriver = jit.JitDriver(
-    greens=['query', 'queryRes'], 
+    greens=['query', 'queryRes', 'printRes'], 
     reds=['rc'],
     )
     # get_printable_location=get_printable_location)
@@ -25,7 +25,7 @@ def run(query, queryRes, printRes):
 
         rc = query.mainloop()
         while rc == CConfig.SQLITE_ROW:
-            jitdriver.jit_merge_point(query=query, queryRes=queryRes, rc=rc)
+            jitdriver.jit_merge_point(query=query, queryRes=queryRes, printRes=printRes, rc=rc)
             if printRes:
                 result = rffi.charpsize2str(rffi.cast(rffi.CCHARP, query.python_sqlite3_column_text(0)), query.python_sqlite3_column_bytes(0))
                 i = 1
