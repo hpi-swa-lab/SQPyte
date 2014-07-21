74b70aaf1ddb34c2847f6deb970b19084dfe14c6
Darya Kurilova <darya@cs.cmu.edu>
2014-07-21 17:08:36 +0100

    Script to test TPC-H queries.
    Former-commit-id: 0ca512f338eec33f85553b2e18fdc318ccc4f7eb

---
+++ testrun.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DIR=$(pwd)
+
+mkdir $DIR/tmp
+
+for i in {1..22}
+do
+    ./target-c -t $DIR/tpch/sqlite-queries-in/$i.sql > $DIR/tmp/$i.out
+
+    DIFF=$(diff $DIR/tmp/$i.out $DIR/tpch/sqlite-queries-out/$i.txt) 
+    if [ "$DIFF" == "" ]; then
+        echo -e "Query $i:\tPass"
+    else
+        echo -e "Query $i:\tFAIL"
+        hg revert $DIR/sqpyte/test/tpch.db
+        rm $DIR/sqpyte/test/tpch.db.orig
+    fi
+done
+
+rm -rf $DIR/tmp
