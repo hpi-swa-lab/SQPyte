20580bfebe7b1ccf4165376419e7ee52776394f2
Darya Kurilova <darya@cs.cmu.edu>
2014-07-22 16:51:02 +0100

    Modified testing and benchmarking scripts to account for pecularities of query 15.
    Former-commit-id: 47bf8dccaabfaf2b528d9dd64a8cd5df58bdfa89

--- benchrun.sh
+++ benchrun.sh
@@ -5,8 +5,18 @@ DB=$DIR/sqpyte/test/big-tpch.db
 for i in {1..14} {16..22}
 do
     echo "========== Query $i =========="
-    for j in {1..10}
-    do
-        ./target-c -b $DB $DIR/tpch/sqlite-queries-in/$i.sql
-    done
+    if [ $i == 15 ]; then
+        for j in {1..10}
+        do
+            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/$i.sql
+        done
+    else
+        # Query 15 might leave the database in an inconsistent state!
+        for j in {1..10}
+        do
+            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/15-view-create.sql
+            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/15-query.sql
+            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/15-view-delete.sql
+        done
+    fi        
 done
--- testrun.sh
+++ testrun.sh
@@ -6,19 +6,29 @@ mkdir $DIR/tmp
 
 for i in {1..22}
 do
-    ./target-c -t $DB $DIR/tpch/sqlite-queries-in/$i.sql > $DIR/tmp/$i.out
+    if [ $i != 15 ]; then
+        ./target-c -t $DB $DIR/tpch/sqlite-queries-in/$i.sql > $DIR/tmp/$i.out
+    else
+        ./target-c -t $DB $DIR/tpch/sqlite-queries-in/15-view-create.sql
+        ./target-c -t $DB $DIR/tpch/sqlite-queries-in/15-query.sql > $DIR/tmp/15.out
+        ./target-c -t $DB $DIR/tpch/sqlite-queries-in/15-view-delete.sql
+    fi
 
     DIFF=$(diff $DIR/tmp/$i.out $DIR/tpch/sqlite-queries-out/$i.txt) 
     if [ "$DIFF" == "" ]; then
         echo -e "Query $i:\tPass"
     else
         echo -e "Query $i:\tFAIL"
-        HG=$(hg st $DB)
-        if [ "$HG" != "" ]; then
-            hg revert $DB
-            rm $DB.orig
-        fi
     fi
+
+    HG=$(hg st $DB)
+    if [ "$HG" != "" ]; then
+        echo -e "*** Reverted database to that in hg. ***"
+        hg revert $DB
+        rm $DB.orig
+        rm $DB-journal
+    fi
+
 done
 
 rm -rf $DIR/tmp
