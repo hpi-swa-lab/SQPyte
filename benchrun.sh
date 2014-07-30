7e7614f8aaa2698d123aa932a54582eead86679b
Darya Kurilova <darya@cs.cmu.edu>
2014-07-30 09:16:18 +0200

    Minor fix to benchmarking script.
    Former-commit-id: ec8ab7081ba853ce1f743187f3189fa3ddf2a382

--- benchrun.sh
+++ benchrun.sh
@@ -5,7 +5,7 @@ DB=$DIR/sqpyte/test/big-tpch.db
 for i in {1..14} {16..22}
 do
     echo "========== Query $i =========="
-    if [ $i == 15 ]; then
+    if [ $i != 15 ]; then
         for j in {1..10}
         do
             ./target-c -b $DB $DIR/tpch/sqlite-queries-in/$i.sql
