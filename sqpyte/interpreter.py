b76dc1556f433abc405e365440f6d992c900f57b
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-08-17 18:17:27 +0200

    disable SCopy until the JIT problem with it is fixed
    Former-commit-id: a0888cda33b3deea016d71d05185126ce9301e1f

--- sqpyte/interpreter.py
+++ sqpyte/interpreter.py
@@ -240,8 +240,8 @@ class Sqlite3Query(object):
         return translated.python_OP_Once(self, pc, op)
 
     def python_OP_SCopy(self, op):
-        # capi.impl_OP_SCopy(self.p, op.pOp)
-        translated.python_OP_SCopy(self, op)
+        capi.impl_OP_SCopy(self.p, op.pOp)
+        # translated.python_OP_SCopy(self, op)
 
     def python_OP_Affinity(self, op):
         # capi.impl_OP_Affinity(self.p, self.db, op.pOp)
