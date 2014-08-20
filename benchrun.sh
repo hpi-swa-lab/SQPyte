ecf46c0a42e4f13bea56d156446afc9d3156c498
Darya Kurilova <darya@cs.cmu.edu>
2014-08-20 14:58:44 +0100

    Added warmup parameter to benchmarking script.
    Former-commit-id: 279b5791eccecec02a92100c8f48e6e21f99ced5

--- benchrun.sh
+++ benchrun.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-DIR=$(pwd)
-DB=$DIR/sqpyte/test/big-tpch.db
 
-for i in {1..14} {16..22}
+WARMUP=5
+SQPYTE_DIR=$(pwd)
+DB=$SQPYTE_DIR/sqpyte/test/big-tpch.db
+
+for i in {1..22}
 do
     echo "========== Query $i =========="
     if [ $i != 15 ]; then
         for j in {1..10}
         do
-            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/$i.sql
+            ./target-c -b $WARMUP $DB $SQPYTE_DIR/tpch/sqlite-queries-in/$i.sql
         done
     else
-        # Query 15 might leave the database in an inconsistent state!
+        # XXX Query 15 currently leaves behind a view so this may fail,
+        # but it won't affect the main query execution.
+        ./target-c -b $DB $SQPYTE_DIR/tpch/sqlite-queries-in/15-view-create.sql > /dev/null
         for j in {1..10}
         do
-            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/15-view-create.sql
-            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/15-query.sql
-            ./target-c -b $DB $DIR/tpch/sqlite-queries-in/15-view-delete.sql
+            ./target-c -b $WARMUP $DB $SQPYTE_DIR/tpch/sqlite-queries-in/15-query.sql
         done
+        # XXX Deleting a view currently doesn't work.
+        # ./target-c -b $DB $SQPYTE_DIR/tpch/sqlite-queries-in/15-view-delete.sql > /dev/null
     fi        
 done
