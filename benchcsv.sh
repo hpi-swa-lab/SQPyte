33ea09389202ae10b0e3da2e4d1e48f02ce81e38
Darya Kurilova <darya@cs.cmu.edu>
2014-08-21 11:07:59 +0100

    Minor renaming.
    Former-commit-id: a013262417d8167410de23f74da1a9dd0ff40020

--- benchcsv.sh
+++ benchcsv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./benchrun.sh | tee tmp.txt
-sed -E '/^[0-9]|^=/!d' tmp.txt | sed 's/$/,/' | sed 's/Query /aquery-/' | tr -d ' ' | tr -d '\n' | tr -d '=' | tr 'a' '\n' | sed '/^$/d' | sed 's/,$//' > benchres.csv
-rm tmp.txt
+./benchrun.sh | tee tmp-bench-sqpyte.txt
+sed -E '/^[0-9]|^=/!d' tmp-bench-sqpyte.txt | sed 's/$/,/' | sed 's/Query /aquery-/' | tr -d ' ' | tr -d '\n' | tr -d '=' | tr 'a' '\n' | sed '/^$/d' | sed 's/,$//' > bench-res.csv
+rm tmp-bench-sqpyte.txt
